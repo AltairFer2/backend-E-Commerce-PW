42c931598f0c505798408a6c8d35ae528672502c
AltairFer2 <20031003@itcelaya.edu>
2024-05-12 20:29:24 -0600

    Creacion de estructura básica del Backend

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const userRoutes = require('./routes/userRoutes');
+
+dotenv.config();
+
+connectDB();
+
 const app = express();
-const port = 3000;
 
-// Importar las rutas
-const indexRouter = require('./routes/index');
+app.use(express.json());
 
-// Usar las rutas
-app.use('/', indexRouter);
+app.use('/api/users', userRoutes);
 
-app.listen(port, () => {
-    console.log(`Servidor escuchando en http://localhost:${port}`);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
