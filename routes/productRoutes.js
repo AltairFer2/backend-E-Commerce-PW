57f3c7e3171fe5294b4ac5cb9c6f6a8d1a5d86b3
Alejandro Capilla Sanchez <19031028@itcelaya.edu.mx>
2024-05-24 10:26:13 -0600

    filtra productos

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -8,7 +8,7 @@ const router = express.Router();
 router.post('/product', createProduct);
 router.get('/product/:id', getProduct);
 router.get('/products/', getProducts);
-router.get('/products/:category', getProducts);
+router.get('/products/:category', getProductsByCategory);
 router.put('/product/:id', updateProduct);
 router.delete('/product/:id', deleteProduct);
 
